0624bf91f2db007ae8e2a1c7a50af7b15d1cf757
Saugat Giri <64838317+saugat-15@users.noreply.github.com>
2025-12-27 04:12:09 +0000

    first event hub send and receive project using node

---
+++ send.js
@@ -0,0 +1,29 @@
+const { EventHubProducerClient } = require("@azure/event-hubs");
+const process = require("process");
+
+const connectionString = process.env.EVENT_HUB_CONNECTION_STRING;
+const eventHubName = process.env.EVENT_HUB_NAME;
+
+async function main() {
+
+  // Create a producer client to send messages to the event hub.
+  const producer = new EventHubProducerClient(connectionString, eventHubName);
+
+  // Prepare a batch of three events.
+  const batch = await producer.createBatch();
+  batch.tryAdd({ body: "First event" });
+  batch.tryAdd({ body: "Second event" });
+  batch.tryAdd({ body: "Third event" });    
+
+  // Send the batch to the event hub.
+  await producer.sendBatch(batch);
+
+  // Close the producer client.
+  await producer.close();
+
+  console.log("A batch of three events have been sent to the event hub");
+}
+
+main().catch((err) => {
+  console.log("Error occurred: ", err);
+});
